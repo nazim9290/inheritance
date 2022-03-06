92cca57a449c60e8e79caf500de0ef631bbc7210
Nazim Uddin <nushourav4@gmail.com>
2022-03-07 08:22:17 +0900

    git adding

--- inheritance/src/Cylinder.java
+++ inheritance/src/Cylinder.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Cylinder {
+public class Cylinder extends Circle{
+    public double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        if(height<0){
+            this.height = 0;
+        }else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    public double getVolume(){
+        return getArea()*height;
+    }
+
 }
